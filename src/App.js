07448b53d0adaa17ebafd1563d3cbfae6771a4ec
Jesse Tomchak <jtomchak@gmail.com>
2017-12-19 08:52:04 -0700

    Updated to the beginning

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+
+const HEROES = [
+  { id: 11, name: "Mr. Nice" },
+  { id: 12, name: "Narco" },
+  { id: 13, name: "Bombasto" },
+  { id: 14, name: "Celeritas" },
+  { id: 15, name: "Magneta" },
+  { id: 16, name: "RubberMan" },
+  { id: 17, name: "Dynama" },
+  { id: 18, name: "Dr IQ" },
+  { id: 19, name: "Magma" },
+  { id: 20, name: "Tornado" }
+];
+
+const App = () => {
+  return (
+    <div>
+      <h1>React Heroes</h1>
+    </div>
+  );
+};
+
+export default App;
